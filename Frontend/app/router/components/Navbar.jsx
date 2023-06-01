124ea56b60a0d3b4150f63ebeb078298e13849cd
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-05-31 21:54:28 -0400

    Created the Navbar component

---
+++ Frontend/app/router/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Spin Cycle</h1>
+      <a href="/login">Login</a>
+      <a href="/CreateAccount">Create Account</a>
+    </div>
+  );
+};
+
+export default Navbar;
